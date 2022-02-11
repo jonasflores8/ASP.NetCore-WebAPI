a23118b8bcc2a000951e234936dff1105821730b
Jonas-Flores <jonas-silva8@outlook.com>
2022-02-11 15:55:30 -0300

    commit initial

---
+++ ASP.NetCore-WebAPI/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NetCore_WebAPI.Models;
+
+namespace ASP.NetCore_WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		public static List<User> users = new List<User>();
+
+		[HttpGet]
+		public List<User> getNames()
+		{
+			return users;
+		}
+
+		[HttpPost]
+		public void PostNames(string name)
+		{
+			if (!string.IsNullOrEmpty(name))
+				users.Add(new User(name));
+		}
+
+		[HttpDelete]
+		public void DeletNames(string name)
+		{
+			users.RemoveAt(users.IndexOf(users.First(x => x.Name.Equals(name))));
+		}
+	}
+}
---
+++ ASP.NetCore-WebAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP.NetCore_WebAPI.Models
+{
+	public class User
+	{
+		public User(string name)
+		{
+			this.Name = name;
+		}
+
+		public string? Name { get; set; }
+	}
+}
